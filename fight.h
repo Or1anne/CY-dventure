6ff2b28337c3977b0f8d8e9f144b3cc2fa95c465
SPrinceK <133128180+SPrinceK@users.noreply.github.com>
2023-05-24 13:27:53 +0200

    Update fight.h

--- fight.h
+++ fight.h
@@ -1,7 +1,7 @@
 #include "player.h"
 
 #ifndef FIGHT_HEADER
-#define FIGHT_HEADER
+#define FIGHT_HEADER 1
 
 // déclaration des fonctions
 Type fight(Entity *attacker, Entity *target, char * step_name);
