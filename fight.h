67672e3e12666683a602b5255f833343c1233a93
Or1anne <133128206+Or1anne@users.noreply.github.com>
2023-05-24 19:06:11 +0200

    fight.h 2.0

--- fight.h
+++ fight.h
@@ -3,8 +3,11 @@
 #ifndef FIGHT_HEADER
 #define FIGHT_HEADER 1
 
+// déclaration des bibliothèques
+#include "invantory.h" 
+
 // déclaration des fonctions
-Type fight(Entity *attacker, Entity *target, char * step_name);
+Type fight(Entity *attacker, Entity *target, char * step_name, Invantory stock);
 Type fightSetup();
 
 #endif
