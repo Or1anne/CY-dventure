7c524d39e669706c0a68b1d2f3861ce6fd04f81d
Or1anne <133128206+Or1anne@users.noreply.github.com>
2023-05-24 19:36:12 +0200

    save.h 2.0

--- save.h
+++ save.h
@@ -4,6 +4,7 @@
 // déclaration des bibliothèques
 #include "function.h"
 #include "player.h"
+#include "invantory.h"
 
 // déclaration des constantes
 #define FILE_SAVE_NAME "save/Cydventure.save"
@@ -15,11 +16,12 @@ typedef struct Save_Struct
     char step[20];
     Entity monster;
     Entity adventurer;
+    Invantory stock;
     
 } Save_Struct;
 
 int game_save_exist();
-void game_save(char *step, Entity monster, Entity adventurer);
-void game_load(char *step, Entity *monster, Entity *adventurer);
+void game_save(char *step, Entity monster, Entity adventurer, Invantory stock);
+void game_load(char *step, Entity *monster, Entity *adventurer, Invantory *stock);
 
 #endif
